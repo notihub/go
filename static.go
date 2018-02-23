38bda2a0bf7ff7c1719bcb703e970cae1b055af1
GyuJung <pandora@crazyguys.me>
2018-02-23 12:07:16 +0900

    RedisProjectLinkKey 추가

--- static.go
+++ static.go
@@ -4,10 +4,15 @@ const (
 	DatabaseName  = "notihub"
 	RedisDatabase = 2
 
+	// Indexing keys
 	RedisNewsKey                 = "index:project:news"
 	RedisKeywordKey              = "index:keyword"
 	RedisKeywordIndexCompleteKey = "index:keyword:complete"
+	// Notify keys
 	RedisNotifyKey               = "notify:project:complete"
+	// Project link keys
+	RedisProjectLinkKey          = "project:link"
+	RedisProjectLinkRequestKey   = "project:link:request"
 
 	RedisNewKeywordChannel = "index.keyword"
 )
